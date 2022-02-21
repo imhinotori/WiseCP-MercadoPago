d84f229560ec8e92dc4aad40327a15fc439d8ab3
imHinotori <hello@hinotori.moe>
2022-02-21 17:51:28 -0300

    Update

--- pages/payform.php
+++ pages/payform.php
@@ -12,10 +12,7 @@ $item->unit_price = 75;
 $preference->items = array($item);
 $preference->save();*/
 
-print_r($config);
-print_r($lang);
 print_r($checkout_info);
-print_r($checkout);
 ?>
 
 Here you will include the payment form.
